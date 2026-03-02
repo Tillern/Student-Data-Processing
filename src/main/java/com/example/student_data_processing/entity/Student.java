08c14b5613724dfc068990fe984b777ddaed72b5
Tillern <brysonminodi@gmail.com>
2026-03-02 11:45:57 +0300

    added student entity

---
+++ src/main/java/com/example/student_data_processing/entity/Student.java
@@ -0,0 +1,37 @@
+package com.example.student_data_processing.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "students",
+        indexes = {
+                @Index(name = "idx_student_id", columnList = "studentId"),
+                @Index(name = "idx_class", columnList = "studentClass")
+        })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long studentId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate dob;
+
+    @Column(name = "studentClass")
+    private String studentClass;
+
+    private Integer score;
+}
