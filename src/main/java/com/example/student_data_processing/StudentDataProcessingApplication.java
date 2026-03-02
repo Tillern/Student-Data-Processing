c1180d33d11fdc488c5612916fc6d0348ad17a89
Tillern <brysonminodi@gmail.com>
2026-03-02 10:56:06 +0300

    first commit

---
+++ src/main/java/com/example/student_data_processing/StudentDataProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_data_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDataProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDataProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/student_data_processing/StudentDataProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_data_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDataProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
